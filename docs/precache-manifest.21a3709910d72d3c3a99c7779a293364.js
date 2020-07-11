67d4044b141d08208ece37881fc2f8c974f7da5e
John Faber <john.florez25@gmail.com>
2020-07-11 02:06:30 -0500

    added docs

---
+++ docs/precache-manifest.21a3709910d72d3c3a99c7779a293364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c5e052504a34710af3a30bb0b0353f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1457683fb28357681786",
+    "url": "/static/css/main.ee0049d3.chunk.css"
+  },
+  {
+    "revision": "0254ad9233ade98e4f87",
+    "url": "/static/js/2.d88fb5c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d88fb5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1457683fb28357681786",
+    "url": "/static/js/main.6a1d63e6.chunk.js"
+  },
+  {
+    "revision": "2c7e0a9c18916f684e55",
+    "url": "/static/js/runtime-main.3083b03f.js"
+  }
+]);
